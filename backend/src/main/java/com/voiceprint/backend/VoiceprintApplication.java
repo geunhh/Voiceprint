047317f06e8442b4f7d57198a9b34e745b87d484
민태홍 <xoghd32@naver.com>
2025-04-24 13:14:53 +0900

    First commit

---
+++ backend/src/main/java/com/voiceprint/backend/VoiceprintApplication.java
@@ -0,0 +1,13 @@
+package com.S12P31B106.voiceprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceprintApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/S12P31B106/voiceprint/VoiceprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S12P31B106.voiceprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
