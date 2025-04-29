e8d0db12c6fffb143a762351547fcaf23a9d0514
정다인 <wjdekdls04@naver.com>
2025-04-29 14:03:07 +0900

    feat: 챗봇 선택 UI 구현

--- frontend/src/pages/diary/DiaryFriendPage.tsx
+++ frontend/src/pages/diary/DiaryFriendPage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import ChatSelector from "../../components/diaryCreate/ChatSelector";
+import PageTitle from "../../components/PageTitle";
 
 export default function DiaryFriendPage() {
   return (
-    <div className="flex items-center justify-center h-dvh text-3xl font-bold">
-      DiaryFriendPage
+    <div className="flex flex-col items-center min-h-screen bg-white">
+      {/* 90퍼센트만 이용 */}
+      <div className="w-[95%] mx-auto">
+        {/* 상단 타이틀 */}
+        <PageTitle
+          title="대화 친구 선택하기"
+          subtitle="오늘 하루 대화 나눌 친구를 선택해주세요"
+        />
+
+        <ChatSelector />
+      </div>
     </div>
   );
 }
