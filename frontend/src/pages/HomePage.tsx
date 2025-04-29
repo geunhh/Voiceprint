870efb475148f807acbfde51ccf71b81036d8951
hyemin3699 <hyemin3699@naver.com>
2025-04-29 12:57:35 +0900

    feat: 버튼 컴포넌트 구현

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-
 export default function HomePage() {
   return (
+    <div>
     <div className="flex items-center justify-center h-dvh text-3xl font-bold">
       HomePage
+    </div>
+
+
     </div>
   );
 }
