1bfe28480b1a4cdcc42482f99753beb08ec7686a
정다인 <wjdekdls04@naver.com>
2025-04-30 09:01:58 +0900

    feat: 게이지 바 구현

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; // 추가
+import { store } from "./store/store"; //
 import AppRoutes from "./routes/AppRoutes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
