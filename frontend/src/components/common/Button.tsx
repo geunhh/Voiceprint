aa3b5610ebb85d44f16f0e997945f11021d362d6
정다인 <wjdekdls04@naver.com>
2025-05-02 10:46:55 +0900

    [#S12P31B106-135] feat: 텍스트 생성 구현

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,30 +1,42 @@
-import '../../index.css'
+import "../../index.css";
 
 interface ButtonProps {
-    text : string
-    type : "fill" | "line"
-    size : "M" | "L"
-    onClick : () => void
+  text: string;
+  type: "fill" | "line";
+  size: "S" | "M" | "L";
+  onClick: () => void;
 }
 
-const Button = ({text, type,size, onClick} : ButtonProps) => {
+const Button = ({ text, type, size, onClick }: ButtonProps) => {
+  const baseStyle = "rounded-xl";
 
-    const baseStyle = "rounded-xl"
+  const typeStyle =
+    type === "fill"
+      ? "bg-yellow-500 text-white"
+      : "border-2 border-yellow-500 text-yellow-500";
 
-    const typeStyle = type==='fill' ? "bg-yellow-500 text-white":"border-2 border-yellow-500 text-yellow-500"
+  let sizeStyle = "";
+  switch (size) {
+    case "S":
+      sizeStyle = "h-5 px-3 text-xs";
+      break;
+    case "M":
+      sizeStyle = "h-11 w-28 text-sm";
+      break;
+    case "L":
+      sizeStyle = "h-11 w-64 text-base";
+      break;
+  }
 
-    const sizeStyle = size === "M" ? "h-11 w-28 text-sm" : "h-11 w-64 text-base"
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${baseStyle} ${typeStyle} ${sizeStyle}`}
+    >
+      {text}
+    </button>
+  );
+};
 
-
-    return (
-        <button
-            type="button"
-            onClick={onClick}
-            className={`${baseStyle} ${typeStyle} ${sizeStyle}`}
-            >
-            {text}
-        </button>
-    )
-}
-
-export default Button
+export default Button;
