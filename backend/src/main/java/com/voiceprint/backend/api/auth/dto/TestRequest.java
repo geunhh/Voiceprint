7007b4bdae31b0f2f25323132a2982226ef7dd1f
xoghd32 <xoghd32@naver.com>
2025-05-05 18:22:20 +0900

    refactor:oauth성공시 back redirect

---
+++ backend/src/main/java/com/voiceprint/backend/api/auth/dto/TestRequest.java
@@ -0,0 +1,10 @@
+package com.voiceprint.backend.api.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestRequest {
+    String access;
+}
