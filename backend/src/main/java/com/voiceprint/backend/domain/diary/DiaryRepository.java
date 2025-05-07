1bbd5dedb59e4f69102bcacb37441810d20c305b
geunhh <rmsgnl11@naver.com>
2025-05-07 20:43:43 +0900

    [S12P31B106-185]feat:내 일기 조회하기 API 구현

--- backend/src/main/java/com/voiceprint/backend/domain/diary/DiaryRepository.java
+++ backend/src/main/java/com/voiceprint/backend/domain/diary/DiaryRepository.java
@@ -1,8 +1,17 @@
 package com.voiceprint.backend.domain.diary;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    @Query("SELECT d from Diary d where d.user.id = :userId" +
+            " and (:cursor is null or d.id < :cursor) order by d.id desc")
+    List<Diary> findMyDiaries(@Param("userId") Long userId,@Param("cursor") Long cursor, Pageable pageable);
 }
--- backend/src/main/java/com/voiceprint/backend/service/diary/DiaryService.java
+++ backend/src/main/java/com/voiceprint/backend/service/diary/DiaryService.java
@@ -1,6 +1,8 @@
 package com.voiceprint.backend.service.diary;
 
 import com.voiceprint.backend.api.diary.dto.DiaryDetailResponseDTO;
+import com.voiceprint.backend.api.diary.dto.DiaryListWithCursorDTO;
+import com.voiceprint.backend.api.diary.dto.DiarySummaryResponseDTO;
 import com.voiceprint.backend.common.exception.diary.DiaryNotFoundException;
 import com.voiceprint.backend.common.exception.diary.UnauthorizedDiaryAccessException;
 import com.voiceprint.backend.common.exception.user.UserNotFoundException;
@@ -12,10 +14,15 @@ import com.voiceprint.backend.service.auth.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Slf4j
 public class DiaryService {
     private final DiaryRepository diaryRepository;
@@ -45,5 +52,45 @@ public class DiaryService {
                 diary.getUser().getNickname(),
                 diary.getThumbnail() != null ? diary.getThumbnail() : null
         );
+    }
+
+    public DiaryListWithCursorDTO getUserDiaries(HttpServletRequest request, Long cursor, Integer size) {
+        // 유저 정보 추출 및 확인
+        Long userId = authService.getUserIdFromRequest(request);
+        log.debug("userId : {}",userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("유저 정보 확인 불가"));
+
+
+        // 2. size + 1 개 조회 (다음 커서 존재 여부 확인용)
+        PageRequest page = PageRequest.of(0,size+1); // (page Num, page Size)
+
+        List<Diary> diaries = diaryRepository.findMyDiaries(userId, cursor, page);
+
+        // 다음 페이지가 있다면?? : size보다 크면 있음.
+        boolean hasNext = diaries.size() > size;
+
+        // 실제 반환할 목록은 size 개
+        if (hasNext) {
+            diaries = diaries.subList(0,size);
+            log.info("다음 게시물이 존재합니다.");
+        } else {
+            log.info("마지막 게시물입니다.");
+        }
+
+        //nextCursor 설정 : 없으면 null
+        Long nextCursor = hasNext ? diaries.getLast().getId() : null;
+
+        List<DiarySummaryResponseDTO> response = diaries.stream()
+                .map(d -> new DiarySummaryResponseDTO(
+                        d.getId(),
+                        d.getTitle(),
+                        d.getContent(),
+                        d.getEmotion() != null ? d.getEmotion().getName() : null,
+                        d.getCreatedAt().toString(),
+                        d.getThumbnail()
+                )).toList();
+
+        return new DiaryListWithCursorDTO(response, nextCursor);
     }
 }
