4fefee200339569c189ddede6c8cf44052aa9272
민태홍 <xoghd32@naver.com>
2025-05-09 17:43:02 +0900

    [S12P31B106-216]feat:Create group, groupUser Entity

---
+++ backend/src/main/java/com/voiceprint/backend/domain/Group.java
@@ -0,0 +1,59 @@
+package com.voiceprint.backend.domain;
+
+
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "`groups`")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(length = 10)
+    private String invitationCode;
+
+    @Column(length = 512)
+    private String groupImage;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false, updatable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    // 그룹 생성시 자동 적용
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+        if (updatedAt == null) {
+            updatedAt = LocalDateTime.now();
+        }
+    }
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
