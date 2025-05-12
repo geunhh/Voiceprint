6cb887e740873025746be41f366ef9f16962048f
geunhh <rmsgnl11@naver.com>
2025-05-12 12:44:13 +0900

    [S12P31B106-243]feat:유저 알람 개인화 User 및 repository 설정

--- backend/src/main/java/com/voiceprint/backend/domain/auth/User.java
+++ backend/src/main/java/com/voiceprint/backend/domain/auth/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -75,10 +76,17 @@ public class User {
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Diary> diaries = new ArrayList<>();
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "last_chatbot_id")
     private Chatbot lastChatbot;
+
+    // 알림 여부
+    @Column(columnDefinition = "BOOLEAN")
+    private Boolean enableAlarm;
+
+    // 알람 시간 21:00 기본.
+    @Column(columnDefinition = "TIME DEFAULT '21:00'")
+    private LocalTime alarmTime = LocalTime.of(21,0);
 
     @PreUpdate
     public void preUpdate() {
