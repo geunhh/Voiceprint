fe3180b5ff015d09f19ec0a655eb56fd3a5b9e36
geunhh <rmsgnl11@naver.com>
2025-05-18 18:18:52 +0900

    [S12P31B106-297]feat:예외처리 클래스 추가 구현

---
+++ backend/src/main/java/com/voiceprint/backend/common/exception/group/InviteExpiredException.java
@@ -0,0 +1,7 @@
+package com.voiceprint.backend.common.exception.group;
+
+public class InviteExpiredException extends RuntimeException {
+    public InviteExpiredException(String message) {
+        super(message);
+    }
+}
