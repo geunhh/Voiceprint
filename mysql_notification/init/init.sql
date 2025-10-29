65e76709a980e5a096da588f1abb6a7d0f707f02
geunhh <rmsgnl111@gamil.com>
2025-10-29 23:47:53 +0900

    feat: prometheus - notification-exporter 추가

--- mysql_notification/init/init.sql
+++ mysql_notification/init/init.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE IF NOT EXISTS notification_db DEFAULT CHARACTER SET utf8mb4;
+
+-- Create a exporter 전용 유저.
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporter_password_notif';
+
+-- 최소 권한 부여
+GRANT SELECT, PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
+FLUSH PRIVILEGES;
+
+-- 확인
+SELECT User, Host, plugin FROM mysql.user WHERE User='exporter';
+SHOW GRANTS FOR 'exporter'@'%';
